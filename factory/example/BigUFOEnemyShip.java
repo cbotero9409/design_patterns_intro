c033cf51d8331b519af2fb54da6bd83334eec3b1
Camilo Botero <cbotero2709@gmail.com>
2025-01-24 15:51:41 -0500

    adds factory example

---
+++ factory/example/BigUFOEnemyShip.java
@@ -0,0 +1,8 @@
+package factory.example;
+
+public class BigUFOEnemyShip extends UFOEnemyShip {
+  public BigUFOEnemyShip() {
+    setName("Big UFO Enemy Ship");
+    setDamage(30.0);
+  }
+}
---
+++ factory/example/UFOEnemyShip.java
@@ -0,0 +1,8 @@
+package factory.example;
+
+public class UFOEnemyShip extends EnemyShip {
+  public UFOEnemyShip() {
+    setName("UFO Enemy Ship");
+    setDamage(20.0);
+  }
+}
