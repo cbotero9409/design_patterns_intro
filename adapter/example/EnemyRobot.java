3616fbe4616f28397854a5c7c22be7c6ac996f3e
Camilo Botero <cbotero2709@gmail.com>
2025-02-04 14:30:36 -0500

    add adapter pattern

---
+++ adapter/example/EnemyRobot.java
@@ -0,0 +1,15 @@
+public class EnemyRobot {
+
+  public void smashWithHands() {
+    System.out.println("Enemy Robot causes 6 damage with its hands");
+  }
+
+  public void walkForward() {
+    System.out.println("Enemy Robot walks forward 5 spaces");
+  }
+
+  public void reactToHuman(String driverName) {
+    System.out.println("Enemy Robot tramps on " + driverName);
+  }
+  
+}
